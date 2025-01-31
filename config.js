ace1e2f40d725c993e2381795ed653dea1fbb8a7
DevByRayRay <raymon@byrayray.dev>
2025-01-31 20:52:45 +0100

    Remove config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-// API Key (replace with your personal API key)
-const cryptoApiKey = 'your_api_key'
-
-// Stock symbol to check (e.g., BTC/USD, ETH/USD)
-const cryptoSymbol = 'BTC/USD'
-
-// Configuration options:
-// - startFrom: Date object or string
-// - interval: Time interval for data collection (e.g., 1D, 1H)
