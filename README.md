ffb4667ccd739d16663c61064d6fa5e3130b44f9
DevByRayRay <raymon@byrayray.dev>
2025-02-01 09:26:09 +0100

    Update

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# financial-market-checker
+# Financial Market Checker
+
+1. Install NodeJS
+2. In the terminal run `npm install`
+3. After that run `node app.js` and there should be a result like this:
+
+| Timestamp         | Open        | High        | Low         | Close       | Diff      | Diff % | Volume     |
+| ----------------- | ----------- | ----------- | ----------- | ----------- | --------- | ------ | ---------- |
+| about 4 hours ago | € 98.720,00 | € 98.948,00 | € 98.604,00 | € 98.781,00 | +€ 197,00 | +0.20% | 2.72081261 |
+| about 3 hours ago | € 98.780,00 | € 99.034,00 | € 98.722,00 | € 98.933,00 | +€ 152,00 | +0.15% | 4.69083087 |
+| about 2 hours ago | € 98.946,00 | € 99.022,00 | € 98.643,00 | € 98.864,00 | € -69,00  | -0.07% | 4.94676898 |
+| about 1 hour ago  | € 98.863,00 | € 98.941,00 | € 98.600,00 | € 98.616,00 | € -248,00 | -0.25% | 6.95339241 |
+| 18 minutes ago    | € 98.616,00 | € 98.731,00 | € 98.574,00 | € 98.584,00 | € -32,00  | -0.03% | 3.25533777 |
--- app.js
+++ app.js
@@ -11,32 +11,28 @@ bitvavo.candles('BTC-EUR', '1h', {}, (error, response) => {
 	if (error === null) {
 		let previousClose = null
 
+		// Print table header
+		console.log('| Timestamp | Open | High | Low | Close | Diff | Volume |')
+		console.log('|-----------|------|------|-----|-------|------|---------|')
+
 		for (let entry of response) {
 			const currentClose = entry[4]
-			let diffStr = ''
+			let diffStr = '-'
 
 			if (previousClose !== null) {
 				const diff = currentClose - previousClose
 				const percentChange = (diff / previousClose) * 100
 				const sign = diff >= 0 ? '+' : ''
 				const color = diff >= 0 ? '\x1b[32m' : '\x1b[31m' // Green for positive, red for negative
-				diffStr = `${color}(${sign}${euroFormatter.format(diff)} / ${sign}${percentChange.toFixed(2)}%)\x1b[0m`
+				diffStr = `${color}${sign}${euroFormatter.format(diff)} / ${sign}${percentChange.toFixed(2)}%\x1b[0m`
 			}
 
 			console.log(
-				'Timestamp: ',
-				dateFns.format(new Date(entry[0]), 'dd-MM-yyyy HH:mm:ss'),
-				' Open: ',
-				euroFormatter.format(entry[1]),
-				' High: ',
-				euroFormatter.format(entry[2]),
-				' Low: ',
-				euroFormatter.format(entry[3]),
-				' Close: ',
-				euroFormatter.format(entry[4]),
-				diffStr,
-				' Volume: ',
-				entry[5]
+				`| ${dateFns.format(new Date(entry[0]), 'dd-MM-yyyy HH:mm:ss')} | ${euroFormatter.format(
+					entry[1]
+				)} | ${euroFormatter.format(entry[2])} | ${euroFormatter.format(entry[3])} | ${euroFormatter.format(
+					entry[4]
+				)} | ${diffStr} | ${entry[5]} |`
 			)
 
 			previousClose = currentClose
